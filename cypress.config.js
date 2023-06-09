0a35a6cb727a5e4154759fbdbb3e5babb9f252df
Carlos Vibanco <cvibancoo@psl.com.co>
2023-06-08 21:54:50 -0500

    cypress set up

---
+++ cypress.config.js
@@ -0,0 +1,39 @@
+const { defineConfig } = require("cypress");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
+
+async function setupNodeEvents(on, config) {
+    await preprocessor.addCucumberPreprocessorPlugin(on, config);
+    on("file:preprocessor", browserify.default(config, {
+        typescript: require.resolve("typescript")
+        })
+    );
+
+    const envName = config.env.envName || 'local';
+    const environmentFilename = `.env/${envName}.settings.json`;
+    const settings = require(environmentFilename);
+
+    if(settings.baseUrl) {
+        config.baseUrl = settings.baseUrl;
+    }
+
+    if(settings.env) {
+        config.env = {
+            ...config.env,
+            ...settings.env,
+        }
+    }
+    return config;
+}
+
+module.exports = defineConfig({
+    projectId: 'xha1xy',
+    e2e: {
+        specPattern: "**/*.feature",
+        setupNodeEvents,
+    },
+    video: false,
+    defaultCommandTimeout: 4000,
+    responseTimeout: 4000,
+    requestTimeout: 4000
+});
